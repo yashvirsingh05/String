86dca6283cecb8db8689a9b0cbb04f900a90b26a
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-11 07:19:02 -0700

    complete

--- String.java
+++ String.java
@@ -1,2 +1,33 @@
-public class String {
+class String1 {
+    public static void main(String[] agrs) {
+        //CharAt method
+        String n = "howtodojava";
+        char c = n.charAt(0);
+        System.out.println(c);
+        for (int i = 0; i < n.length(); i++) {
+            c = n.charAt(i);
+
+            System.out.print(c + " ");
+        }
+        System.out.println(" ");
+        // concat method
+        {
+            System.out.println(n.concat(" good programe"));
+        }
+        // CompareTOIgnoreCase method
+
+        {
+            String a = "howtodojavaprograme";
+            System.out.println(n.compareToIgnoreCase(a));
+        }
+        //index Of method
+        {
+            System.out.println(n.indexOf('j'));
+        }
+        // replace method
+        {
+            System.out.println(n.replace('j', 'J'));
+            System.out.println(n.replaceAll(n, "javabest"));
+        }
+    }
 }
